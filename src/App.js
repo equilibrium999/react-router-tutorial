4683e7cf18bbc853123f22127cdedb22122e9b8d
Minh Tri Le <minhtri.manchester@gmail.com>
2019-03-25 22:20:05 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          {/* Menu */}
+          <nav className="navbar navbar-inverse">
+            <ul className="nav navbar-nav">
+              <li>
+                <Link to="/" className="my-link">Home Page</Link>
+              </li>
+              <li>
+                <Link to="/about" className="my-link">About</Link>
+              </li>
+              <li>
+                <Link to="/contact" className="my-link">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+          {/* Content */}
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Contact}/>
+        </div>
+      </Router>
     );
   }
 }
